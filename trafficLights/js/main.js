5d44061891e5b25ada7ded6ffb4c778b53e4cc95
Annaa <32773252+AnnaA123@users.noreply.github.com>
2019-02-12 14:29:56 +0200

    Update main.js

--- trafficLights/js/main.js
+++ trafficLights/js/main.js
@@ -12,24 +12,24 @@ const redCount = document.querySelector("#redCount");
 const yellowCount = document.querySelector("#yellowCount");
 const greenCount = document.querySelector("#greenCount");
 
-
+//"The colour is" segment is always gonna be rgb(0, 0, 0) (because the light goes out when you hover over it), but it does work
 redLight.addEventListener('click', (evt) => {
   youClicked.innerHTML = 'You clicked the red lamp';
-  theColour.innerHTML = 'The colour is red';
+  theColour.innerHTML = 'The colour is ' + window.getComputedStyle(redLight, null).getPropertyValue('background-color');
   redClicks++;
   redCount.innerHTML = 'Red: ' + redClicks;
 });
 
 yellowLight.addEventListener('click', (evt) => {
   youClicked.innerHTML = 'You clicked the yellow lamp';
-  theColour.innerHTML = 'The colour is yellow';
+  theColour.innerHTML = 'The colour is ' + window.getComputedStyle(yellowLight, null).getPropertyValue('background-color');
   yellowClicks++;
   yellowCount.innerHTML = 'Yellow: ' + yellowClicks;
 });
 
 greenLight.addEventListener('click', (evt) => {
   youClicked.innerHTML = 'You clicked the green lamp';
-  theColour.innerHTML = 'The colour is green';
+  theColour.innerHTML = 'The colour is ' + window.getComputedStyle(greenLight, null).getPropertyValue('background-color');
   greenClicks++;
   greenCount.innerHTML = 'Green: ' + greenClicks;
 });
